79b85908c1fed7250f197cc4f5df7fd2898d0f1f
Quenie-Ann <abragan.quenieann@gmail.com>
2026-03-02 10:16:04 +0800

    Fix vite base path for vercel

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  // If on Vercel, use '/', otherwise use the subfolder
+  // If on Vercel, use '/', otherwise use the local folder
   base: process.env.VERCEL ? '/' : '/skill-link-cdo/',
 }))
